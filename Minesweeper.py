95ba5e4be87f7df952f00751255e1003114a82c5
hurricanzy <savateev.da@dvfu.ru>
2024-07-22 16:01:18 +1000

    add a field(поле)

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,3 +1,21 @@
-print('qq')
-x=5
-print(x)
+import tkinter as tk
+window=tk.Tk()
+
+ROW=5
+COLUMNS=3
+buttons=[]
+for i in range(ROW):
+    temp=[]
+    for j in range(COLUMNS):
+        btn=tk.Button(window, width=5, font= 'Calibri 15 bold')
+        temp.append(btn)
+    buttons.append(temp)
+for ROW_btn in buttons:
+    print(ROW_btn)
+
+for i in range(ROW):
+    for j in range(COLUMNS):
+        btn=buttons[i][j]
+        btn.grid(row=i,column=j)
+
+window.mainloop()
