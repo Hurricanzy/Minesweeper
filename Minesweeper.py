c8d39f57d45b0ede869cd8eb67a1138aeb8367ae
hurricanzy <savateev.da@dvfu.ru>
2024-07-22 16:40:48 +1000

    add a class MineSweeper

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,21 +1,35 @@
 import tkinter as tk
-window=tk.Tk()
 
-ROW=5
-COLUMNS=3
-buttons=[]
-for i in range(ROW):
-    temp=[]
-    for j in range(COLUMNS):
-        btn=tk.Button(window, width=5, font= 'Calibri 15 bold')
-        temp.append(btn)
-    buttons.append(temp)
-for ROW_btn in buttons:
-    print(ROW_btn)
 
-for i in range(ROW):
-    for j in range(COLUMNS):
-        btn=buttons[i][j]
-        btn.grid(row=i,column=j)
+class MineSweeper:
+    window=tk.Tk()
+    ROW=5
+    COLUMNS=3
+    def __init__(self):
+        self.buttons = []
+        for i in range(MineSweeper.ROW):
+            temp = []
+            for j in range(MineSweeper.COLUMNS):
+                btn = tk.Button(MineSweeper.window, width=5, font= 'Calibri 15 bold')
+                temp.append(btn)
+            self.buttons.append(temp)
+    def create_widgets(self):
+        for i in range(MineSweeper.ROW):
+            for j in range(MineSweeper.COLUMNS):
+                btn = self.buttons[i][j]
+                btn.grid(row=i, column=j)
 
-window.mainloop()
+    def start(self):
+        self.create_widgets()
+        self.print_buttons()
+        MineSweeper.window.mainloop()
+
+    def print_buttons(self):
+        for ROW_btn in self.buttons:
+            print(ROW_btn)
+
+
+game = MineSweeper()
+game.start()
+
+
